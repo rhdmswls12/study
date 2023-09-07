202ee7963eb022619dfb2e10134a0dfb864739f6
rhdmswls12 <rhgusdnr1293@naver.com>
2023-09-07 14:41:41 +0900

    할당 단언(Assertion)

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,26 @@
-// 타입 추론(Inference)
+// Assertion
 
-// 1) 초기화된 변수
-// 2) 기본 값이 설정된 매개 변수
-// 3) 반환 값이 있는 함수
-
-// 초기화된 변수 'num'
-let num = 12
-
-// 기본 값이 지정된 매개 변수 'b' + 반환 값이 확실한 함수 'add'
-function add(a: number, b = 12) {
-  return a + b
+// 1)
+const el = document.querySelector('body') as HTMLBodyElement
+if (el) {
+  el.textContent = 'Hello world'
 }
+
+// 2)
+function getNumber(x: number | null | undefined) {
+  if (x) {
+    return Number(x.toFixed(2))
+  }
+}
+getNumber(3.141592)
+getNumber(null)
+
+// 3)
+function getValue(x: string | number, isNumber: boolean) {
+  if (isNumber) {
+    return Number((x as number).toFixed(2))
+  }
+  return (x as string).toUpperCase()
+}
+getValue('hello world', false) // 'HELLO WORLD'
+getValue(3.141592, true) // 3.14
