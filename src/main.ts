9b75d08f7826dc20e48fb6d6d1573ef0f1aa0f29
rhdmswls12 <rhgusdnr1293@naver.com>
2023-09-11 22:15:34 +0900

    Overloading

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
-// 함수 - 명시적 this
+// 함수 - 오버로딩(Overloading)
 
-interface Cat {
-  name: string
-  age: number
+// 1)
+function add1(a: string, b: string) {
+  return a + b;
 }
-const cat: Cat = {
-  name: 'Lucy',
-  age: 2
+function add2(a: number, b: number) {
+  return a + b;
 }
+add1('hello ', 'world');
+add2(1, 2);
+add1('hello ', 2) // 에러
+add2('hello ', 2) // 에러
 
-function hello(this: Cat, message: string) {
-  console.log(`Hello ${this.name}, ${message}`) 
+// 2)
+function add(a: string, b: string): string // 첫 번째 타입 선언
+function add(a: number, b: number): number // 두 번째 타입 선언
+function add(a: any, b: any) { // 구현 부분
+  return a + b;
 }
-hello.call(cat, 'You are pretty awesome!')
+add('hello ', 'world'); // 'hello world'
+add(1, 2); // 3
+add('hello ', 2) // 에러
+add('hello ', 2) // 에러
