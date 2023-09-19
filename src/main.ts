be6b3c2a78f7ed6e87b593faa1c137a300df338b
rhdmswls12 <rhgusdnr1293@naver.com>
2023-09-19 15:44:56 +0900

    Generic - function, class

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,42 @@
+// 제네릭(Generic)
+// 함수
+interface Obj {
+  x: number
+}
+type Arr = [number, number]
+
+function toArray<T>(a: any, b: any) {
+  return [a, b]
+}
+function toArray2<T>(a: T, b: T) {
+  return [a, b]
+}
+console.log(
+  toArray<string>('hi', 'hello'),
+  toArray<number>(1, 2),
+  toArray2(true, false),
+  toArray2<Arr>([1, 2], [3, 4])
+)
+
 // 클래스
-
-// 접근 제어자(Access Modifiers)
-// public - 어디서나 자유롭게 접근 가능, 클래스 바디에서 생략 가능(매개변수에서 사용하는 경우에는 생략 불가)
-// protected - 나와 파생된 후손 클래스 내에서 접근 가능
-// private - 내 클래스에서만 접근 가능
-
-class UserA {
-  constructor(
-    public first: string, 
-    public last: string, 
-    public age: number
-    ) {
-  }
-  setAge() {
-    return `${this.first} ${this.last} is ${this.age}`
+class User<P> {
+  constructor(public payload: P) {}
+  getPayload() {
+    return this.payload
   }
 }
-class UserB extends UserA {
-  getAge() {
-    return `${this.first} ${this.last} is ${this.age}` 
-  }
+interface UserAType {
+  name: string
+  age: number
+  isValid: boolean
 }
-class UserC extends UserB {
-  getAge() {
-    return `${this.first} ${this.last} is ${this.age}`
-  }
+interface UserBType {
+  name: string
+  age: number
+  emails: string[]
 }
-
-const neo = new UserA('Neo', 'Kim', 30)
-console.log(neo.first) 
-console.log(neo.last) 
-console.log(neo.age) 
+const apple = new User<UserAType>({
+  name: 'apple',
+  age: 18,
+  isValid: true
+})
